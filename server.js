e2e0d24d745af98816c59631693b2cff65b020fe
kvora2 <kelvinvora21@gmail.com>
2022-10-05 17:20:15 -0400

    initiate

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+
+var path = require("path");
+var HTTP_PORT = process.env.PORT || 8080;
+
+function getit_started() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+
+
+app.get("/", function (req, res) {
+    var html;
+    html = "<h1> Declaration (Instructions: text size in heading 2):</h1>"
+    html += "<br>Name: Kelvin Vora<br><br>Student Number: 157616210<br><br>"
+    html += "<a style=font-size:20px href= /test1>Go to test1</az>"
+
+    res.send(html);
+});
+
+app.get("/test1", function(req, res) {
+    res.sendFile(path.join(__dirname, "./views/test1.html"));
+});
+
+app.get("/test1", function(req, res) {
+    res.sendFile(path.join(__dirname, "./views/test1.html"));
+});
+
+
+// app.get("/home", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/"));
+// });
+
+app.listen(HTTP_PORT, getit_started);
